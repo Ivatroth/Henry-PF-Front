9aec976d82571cbf6c995ef23297c8e5d2e3bad3
ivatroth <ivana.frontroth@gmail.com>
2023-06-05 10:52:33 -0300

    deploy

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,10 @@ import "./index.css";
 import { Provider } from "react-redux";
 import store from "./redux/store.js";
 import { BrowserRouter } from "react-router-dom";
+import axios from "axios";
 
+// axios.defaults.baseURL = "http://localhost:3001"
+axios.defaults.baseURL = "https://proyecto-final-back-production-d376.up.railway.app/"
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <Provider store={store}>
